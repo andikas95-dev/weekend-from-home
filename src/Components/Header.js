e9f6d95b5cf62e4b017e4dc4174879831b31999c
andikas95-dev <andikaperdana95@gmail.com>
2020-07-11 15:31:53 +0700

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <p class="navbar-brand">Navbar</p>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div>test</div>
+            </div>
+        );
+    }
+}
+
+export default Home;
